de064222db38bdbbc8169e8336d87f9add7344ac
Blurgy <gaoyangzhang@outlook.com>
2019-03-31 23:37:14 +0800

    fixing & extending capabilities

--- episodes/episodes.py
+++ episodes/episodes.py
@@ -12,23 +12,32 @@ import socket
 import struct
 
 
+def is_url(x):
+    return not not re.match(r'^https?://\w.+$', x);
+def split_host(x):
+    return re.findall(r'(https?://.*?)/', x)[0];
+def is_ts(x):
+    return not not re.match(r'.*\.ts$', x);
+def is_m3u8(x):
+    return not not re.match(r'.*\.m3u8', x);
 def create_headers():
     try:
         ip = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
         headers = {
-            'Cookies': "UM_distinctid=169ce771d23748-0814577fe36b88-15231708-1fa400-169ce771d24747; PHPSESSID=bltpfnmosonbp0tvb9c2ch96q5; CNZZDATA1261174353=2079538977-1553945734-%7C1553951134",
+            # 'Cookies': "UM_distinctid=169ce771d23748-0814577fe36b88-15231708-1fa400-169ce771d24747; PHPSESSID=bltpfnmosonbp0tvb9c2ch96q5; CNZZDATA1261174353=2079538977-1553945734-%7C1553951134",
+            # 'Cookies': "XSRF-TOKEN=eyJpdiI6IjZkZkJSZFZta1RqZlU0VTZkZ1hOdlE9PSIsInZhbHVlIjoiV3RpQkE3bVc0WEY2QmdnSDFNcmtWb051WDhmNUc3MTg2OHBsY0ZrQmRLVmhpM1RMNlB0UnlYb3JmM3hFRHRVUCIsIm1hYyI6ImI1Nzk3Yzg3YjYzYmViZWRhZDgzMjc5NTNkNWMzNTcxMjVlZjAwZjA2ZDNmODIyZjE3MjNjMWZmZDVjZWIwNzQifQ%3D%3D; laravel_session=eyJpdiI6Ik1BN1wvenJ6dUdHdmhuK2JwVDF4OU5BPT0iLCJ2YWx1ZSI6InlXdFE5ZitVbml1UEhrNDhaZWoxRHAySCs4K2JzbDRlaktNa2pyU0ZoTXdlSWZ6aXBiMnFpNHVRNXBJS2ZQQ3YiLCJtYWMiOiI4YWM3YzNjZjRmZTQyNjllNGE3Y2FlMTI3MzkxZGIyNjk2YTRlZGQ3NGQyODIyMDU3YzlmNmUwMDMzM2MwMjMzIn0%3D",
             'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36",
-            'CLIENT-IP': ip,
-            'X-FORWARDED-FOR': ip
+            'CLIENT-IP': ip
+            # 'X-FORWARDED-FOR': ip
         }
         return headers;
     except Exception as e:
         print("episodes.py::create_headers() error: %s" % e);
         return None;
 
-def get_content(url, headers = create_headers(), proxies = {'http': "http://61.184.109.33:61320", 'https': "https://210.5.10.87:53281"}, ):
+def get_content(url, headers = create_headers(), proxies = {'http': "http://61.184.109.33:61320", 'https': "https://210.5.10.87:53281"}, timeout = 9.9, ):
     try:
-        content = requests.get(url, headers = headers, timeout = 5.0).text;
+        content = requests.get(url, headers = headers, timeout = timeout).text;
         return html.unescape(content.encode('latin-1', 'ignore').decode('utf-8', 'ignore'));
     except Exception as e:
         print("episodes.py::get_content() error: %s" % e);
@@ -42,16 +51,56 @@ class episode:
             self.epid = _epid;
             self.from_series = _from_series;
             self.tvid = None;
+            self.m3u8 = None;
         except Exception as e:
             print("episodes.py::episode::__init__() error: %s" % e);
 
     def g_tvid(self, ):
         try:
             content = get_content(self.base_url);
-            self.tvid = re.findall(r'tvid="(.*/)(.*?m3u8.*?)"', content)[0];
+            if(re.search(r'tvid="(.*/)(.*?m3u8.*?)"', content)):
+                self.tvid = re.findall(r'tvid="(.*/)(.*?m3u8).*?"', content)[0];
+            else:
+                self.tvid = re.findall(r'tvid="(.*?)"', content)[0];
+                self.tvid = re.findall(r'(.*/)(\w+)', self.tvid)[0];
             return self.tvid;
         except Exception as e:
             print("episodes.py::episode::g_tvid() error: %s" % e);
+            return None;
+
+    def m3u8_unify(self, url = None):
+        try:
+            if(url == None):
+                url = self.tvid[0] + self.tvid[1];
+            if(not is_url(url)):
+                print("no url found during download, abort");
+                return None;
+            if(is_m3u8(url)):
+                content = get_content(url);
+                nurl = url;
+                for line in content.splitlines():
+                    if(re.match(r'^#(.*?)$', line)):
+                        continue;
+                    if(is_m3u8(line)):
+                        nurl = re.sub(url.split('/')[len(url.split('/'))-1], line, url);
+                        content = get_content(nurl);
+                        break;
+                    else:
+                        break;
+                self.m3u8 = "";
+                for line in content.splitlines():
+                    if(not is_url(line) and is_ts(line)):
+                        line = re.sub(line, re.sub(nurl.split('/')[len(nurl.split('/'))-1], line, nurl), line);
+                    self.m3u8 += line + '\n';
+            else:
+                content = get_content(url);
+                nurl = re.findall(r'"(.*?m3u8)', content)[0];
+                if(not is_url(nurl)):
+                    nurl = split_host(url) + nurl;
+                self.m3u8_unify(nurl);
+            return self.m3u8;
+        except Exception as e:
+            print("episodes.py::episodes::m3u8_unify() error: %s" % e);
             return None;
 
     def Download(self, ):   # as *.m3u8
@@ -61,14 +110,15 @@ class episode:
                 os.makedirs(self.from_series.sname);
             fname = self.from_series.sname + '/' + str(self.epid) + ".m3u8";
             if(os.path.exists(fname)):
-                print("file [%s] already exists, skipping");
+                print("file [%s] already exists, skipping..." % fname);
                 return False;
-            m3u8 = get_content(self.tvid[0] + self.tvid[1]);
-            m3u8 = re.sub(r'[\n]out', lambda x: '\n' + self.tvid[0] + x.group().strip(), m3u8);
-            with open(fname, 'wb') as f:
-                f.write(m3u8.encode());
-            print("[%s] episode [%d] downloaded at [%s]" % (self.from_series.sname, self.epid, fname));
-            return True;
+            if(self.m3u8_unify()):
+                with open(fname, 'wb') as f:
+                    f.write(self.m3u8.encode());
+                print("[%s] episode [%d] downloaded as [%s]" % (self.from_series.sname, self.epid, fname));
+                return True;
+            else:
+                return False;
         except Exception as e:
             print("episodes.py::episode::Download() error: %s" % e);
             return False;
@@ -184,6 +234,7 @@ class crawl:
 
 if(__name__ == "__main__"):
     try:
+        # print(is_url(input()));
         x = crawl();
         x.search("爱 死亡 机器人");
         x.Download(x.select());
