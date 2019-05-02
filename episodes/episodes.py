372a90e6f8648b3f461cc4717519fae19f7c6f6c
Blurgy <gaoyangzhang@outlook.com>
2019-05-02 20:36:27 +0800

    优化url匹配规则

--- episodes/episodes.py
+++ episodes/episodes.py
@@ -19,7 +19,7 @@ import select
 def do_nothing():
     pass;
 def is_url(x):
-    return not not re.match(r'^https?://\w.+$', x);
+    return not not re.match(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)', x);
 def split_host(x):
     return re.findall(r'(https?://.*?)[/$]', x)[0];
 def is_ts(x):
